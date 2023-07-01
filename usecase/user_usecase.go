645b036bd283d4036b6d8a2c1e2df549543c9c18
Haru Nishiura <h24wler@gmail.com>
2023-07-01 19:17:05 +0900

    Add login method

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -1,9 +1,12 @@
 package usecase
 
 import (
+	"os"
 	"task-manager/model"
 	"task-manager/repository"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -34,4 +37,26 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 		Email: newUser.Email,
 	}
 	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	storedUser := model.User{}
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storedUser.ID,
+		"exp":     time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
