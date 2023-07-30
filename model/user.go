aaeb3ea56c900b72943891a2fa2c0981f806362b
Haru Nishiura <h24wler@gmail.com>
2023-07-30 23:31:33 +0900

    Add http communication secured

--- model/user.go
+++ model/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Email     string    `json:"email" gorm:"unique"`
-	Password  string    `json:"passward"`
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
