71635613c35d3ce0de93c69f2b969f6311b2e6ae
Haru Nishiura <h24wler@gmail.com>
2023-08-11 15:25:13 +0900

    Add custom hooks

--- frontend/src/hooks/useMutateAuth.ts
+++ frontend/src/hooks/useMutateAuth.ts
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
 import useStore from '../store'
 import { Credential } from '../types'
-import { useError } from '../hooks/useError'
+import { useError } from './useError'
 
 export const useMutateAuth = () => {
   const navigate = useNavigate()
